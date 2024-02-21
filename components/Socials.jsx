fb615f13e3dbad9c63b0aff799be5ffc6e093f58
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-02-21 20:51:36 +0330

    header

---
+++ components/Socials.jsx
@@ -0,0 +1,45 @@
+"use client";
+import Link from "next/link"
+import {
+  RiYoutubeFill,
+  RiLinkedinFill,
+  RiGithubFill,
+  RiInstagramFill,
+  RiFacebookFill,
+} from "react-icons/ri";
+const icons=[
+  {
+    path:"/",
+    name:<RiYoutubeFill/>,
+  },
+  {
+    path:"/",
+    name:<RiLinkedinFill/>,
+  },
+  {
+    path:"/",
+    name:<RiGithubFill/>,
+  },
+  {
+    path:"/",
+    name:<RiInstagramFill/>,
+  },
+  {
+    path:"/",
+    name:<RiFacebookFill/>,
+  },
+]
+const Socials = ({containerStyle,iconsStyle}) => {
+  return <div className={`${containerStyle}`}>
+    {icons.map((icon,index)=>{
+      return(
+        <Link href={icon.path} key={index}>
+          <div className={`${iconsStyle}`}>{icon.name}</div>
+        </Link>
+      )
+    })}
+    
+  </div>;
+};
+
+export default Socials;

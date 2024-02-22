08ae3f582dc0380989ff7071028d7e55e8e11f91
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-02-22 22:15:58 +0330

    bg-hero section

--- components/DevImg.jsx
+++ components/DevImg.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import Link from "next/link"
+import Image from "next/image"
 
-const DevImg = () => {
+
+
+const DevImg = ({containerStyle,imgSrc}) => {
   return (
     <div>DevImg</div>
   )
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -51,7 +51,10 @@ const Hero = () => {
               iconsStyle="text-foreground text-[22px] hover:text-primary transition-all"
             />
           </div>
-          <div className="hidden xl:flex relative">Image</div>
+          <div className="hidden xl:flex relative">
+            <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -left-2"></div>
+            <DevImg/>
+          </div>
         </div>
         <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
           <RiArrowDownSLine className="text-3xl text-primary" />

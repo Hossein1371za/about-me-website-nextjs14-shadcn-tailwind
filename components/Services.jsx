0cb0a61cade20eb1c0752abb81d9d89a7521d087
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-02-28 23:45:25 +0330

    end of services section

--- components/Services.jsx
+++ components/Services.jsx
@@ -8,48 +8,58 @@ import {
 } from "@/components/ui/card";
 
 const servicesData = [
-    {
-        icon:<GanttChartSquare size={72} strokeWidth={0.8} />,
-        title:"طراحی وب سایت",
-        description:"طراحان سایت هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند که محتوای اصلی صفحات آماده نیست. در نتیجه طرح کلی دید درستی به کار فرما نمیدهد."
-    },
-    {
-        icon:<Blocks size={72} strokeWidth={0.8} />,
-        title:"توسعه وب ",
-        description:"طراحان سایت هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند که محتوای اصلی صفحات آماده نیست. در نتیجه طرح کلی دید درستی به کار فرما نمیدهد."
-    },
-    {
-        icon:<Gem size={72} strokeWidth={0.8} />,
-        title:"توسعه اپلیکیشن",
-        description:"طراحان سایت هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند که محتوای اصلی صفحات آماده نیست. در نتیجه طرح کلی دید درستی به کار فرما نمیدهد."
-    },
-]
-
+  {
+    icon: <GanttChartSquare size={72} strokeWidth={0.8} />,
+    title: "طراحی وب سایت",
+    description:
+      "طراحان سایت هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند که محتوای اصلی صفحات آماده نیست. در نتیجه طرح کلی دید درستی به کار فرما نمیدهد.",
+  },
+  {
+    icon: <Blocks size={72} strokeWidth={0.8} />,
+    title: "توسعه وب ",
+    description:
+      "طراحان سایت هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند که محتوای اصلی صفحات آماده نیست. در نتیجه طرح کلی دید درستی به کار فرما نمیدهد.",
+  },
+  {
+    icon: <Gem size={72} strokeWidth={0.8} />,
+    title: "توسعه اپلیکیشن",
+    description:
+      "طراحان سایت هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند که محتوای اصلی صفحات آماده نیست. در نتیجه طرح کلی دید درستی به کار فرما نمیدهد.",
+  },
+];
 
 const Services = () => {
-  return <section className='mb-12 xl:mb-36'>
-    <div className="container mx-auto">
-        <h2 className="section-title mb-12 xl:mb-24 text-center mx-auto">خدمات من</h2>
+  return (
+    <section className="mb-12 xl:mb-36">
+      <div className="container mx-auto">
+        <h2 className="section-title mb-12 xl:mb-24 text-center mx-auto">
+          خدمات من
+        </h2>
         <div className="grid xl:grid-cols-3 gap-y-12 xl:gap-y-24 justify-center">
-            {servicesData.map((item,index)=>{
-                return(
-                    <Card className="w-full max-w-[424px] h-[300px] flex flex-col pt-16 pb-10 justify-center items-center relative"
-                    key={index}>
-                        <CardHeader className="text-primary absolute -top-[60px]">
-                            <div className="w-[140px] h-[80px] bg-white dark:bg-background flex justify-center items-center">
-                                {item.icon}
-                            </div>
-                        </CardHeader>
-                        <CardContent className="text-center">
-                            <CardTitle className="mb-4">{item.title}</CardTitle>
-                            <CardDescription className="text-lg">{item.description}</CardDescription>
-                        </CardContent>
-                    </Card>
-                )
-            })}
+          {servicesData.map((item, index) => {
+            return (
+              <Card
+                className="w-full max-w-[424px] h-[300px] flex flex-col pt-16 pb-10 justify-center items-center relative"
+                key={index}
+              >
+                <CardHeader className="text-primary absolute -top-[60px]">
+                  <div className="w-[140px] h-[80px] bg-white dark:bg-background flex justify-center items-center">
+                    {item.icon}
+                  </div>
+                </CardHeader>
+                <CardContent className="text-center">
+                  <CardTitle className="mb-4">{item.title}</CardTitle>
+                  <CardDescription className="text-lg">
+                    {item.description}
+                  </CardDescription>
+                </CardContent>
+              </Card>
+            );
+          })}
         </div>
-    </div>
-  </section>;
+      </div>
+    </section>
+  );
 };
 
 export default Services;

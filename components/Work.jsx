d56071d098c85a6941e290455d3bfd17229ca85c
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-02-29 22:06:33 +0330

    work section

---
+++ components/Work.jsx
@@ -0,0 +1,17 @@
+"use client"
+import Link from "next/link"
+import { Button } from "./ui/button"
+
+import "swiper/css"
+import "swiper/css/pagination"
+
+import {Pagination} from "swiper/modules"
+import ProjectCard from "./ProjectCard"
+
+const Work = () => {
+  return (
+    <div>Work</div>
+  )
+}
+
+export default Work

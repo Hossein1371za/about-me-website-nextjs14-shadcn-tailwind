488f10c1091b0295afa6eea724957bf3f01b9c5d
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-02-29 23:01:51 +0330

    work section

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,17 +1,125 @@
-"use client"
-import Link from "next/link"
-import { Button } from "./ui/button"
+"use client";
+import Link from "next/link";
+import { Button } from "./ui/button";
 
-import "swiper/css"
-import "swiper/css/pagination"
+import {Swiper,SwiperSlide} from "swiper/react"
+import "swiper/css";
+import "swiper/css/pagination";
 
-import {Pagination} from "swiper/modules"
-import ProjectCard from "./ProjectCard"
+import { Pagination } from "swiper/modules";
+import ProjectCard from "./ProjectCard";
+
+const projectData = [
+  {
+    image: "/work/3.png",
+    category: "react js",
+    name: "وب سایت اول",
+    description:
+      "طراحان سایت هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند که محتوای اصلی صفحات آماده نیست. در نتیجه طرح کلی دید درستی به کار فرما نمیدهد.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/4.png",
+    category: "react js",
+    name: "وب سایت دوم",
+    description:
+      "طراحان سایت هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند که محتوای اصلی صفحات آماده نیست. در نتیجه طرح کلی دید درستی به کار فرما نمیدهد.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/2.png",
+    category: "next js",
+    name: "وب سایت سوم",
+    description:
+      "طراحان سایت هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند که محتوای اصلی صفحات آماده نیست. در نتیجه طرح کلی دید درستی به کار فرما نمیدهد.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/1.png",
+    category: "next js",
+    name: "وب سایت چهارم",
+    description:
+      "طراحان سایت هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند که محتوای اصلی صفحات آماده نیست. در نتیجه طرح کلی دید درستی به کار فرما نمیدهد.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/3.png",
+    category: "next js",
+    name: "وب سایت پنجم",
+    description:
+      "طراحان سایت هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند که محتوای اصلی صفحات آماده نیست. در نتیجه طرح کلی دید درستی به کار فرما نمیدهد.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/4.png",
+    category: "next js",
+    name: "وب سایت ششم",
+    description:
+      "طراحان سایت هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند که محتوای اصلی صفحات آماده نیست. در نتیجه طرح کلی دید درستی به کار فرما نمیدهد.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/1.png",
+    category: "fullstack",
+    name: "وب سایت هفتم",
+    description:
+      "طراحان سایت هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند که محتوای اصلی صفحات آماده نیست. در نتیجه طرح کلی دید درستی به کار فرما نمیدهد.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/3.png",
+    category: "fullstack",
+    name: "وب سایت هشتم",
+    description:
+      "طراحان سایت هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند که محتوای اصلی صفحات آماده نیست. در نتیجه طرح کلی دید درستی به کار فرما نمیدهد.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/2.png",
+    category: "react js",
+    name: "وب سایت نهم",
+    description:
+      "طراحان سایت هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند که محتوای اصلی صفحات آماده نیست. در نتیجه طرح کلی دید درستی به کار فرما نمیدهد.",
+    link: "/",
+    github: "/",
+  },
+];
 
 const Work = () => {
   return (
-    <div>Work</div>
-  )
-}
+    <section className="relative mb-12 xl:mb-48">
+      <div className="container mx-auto">
+        <div className="max-w-[400px] mx-auto xl:mx-0 text-center xl:text-right mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start">
+          <h2 className="section-title mb-4">جدیدترین پروژه ها</h2>
+          <p>
+            طراحان سایت هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند
+            که محتوای اصلی صفحات آماده نیست. در نتیجه طرح کلی دید درستی به کار
+            فرما نمیدهد.
+          </p>
+          <Link href="./project">
+            <Button>همه</Button>
+          </Link>
+        </div>
+        <div>
+            <Swiper>
+                {projectData.slice(0.4).map((item,index)=>{
+                    return(
+                        <SwiperSlide key={index}>slide</SwiperSlide>
+                    )
+                })}
+            </Swiper>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Work
+export default Work;

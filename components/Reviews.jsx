07640a0041c340411d1e45dddfa5607b5f0a9c8e
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-03-02 23:26:50 +0330

    end of reviews section

--- components/Reviews.jsx
+++ components/Reviews.jsx
@@ -1,13 +1,14 @@
 "use client";
 import Image from "next/image";
 
-import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
 import "swiper/css/pagination";
 
-import { pagination } from "swiper/modules";
+import { Pagination } from "swiper/modules";
+
 
 const reviewsData = [
   {
@@ -58,26 +59,45 @@ const Reviews = () => {
   return (
     <section className="mb-12 xl:mb-32">
       <div className="container mx-auto">
-        <h2 className="section-title mb-12 text-center">مرورها</h2>
+        <h2 className="section-title mb-12 text-center mx-auto">مرورها</h2>
         <Swiper
-          slidesPerview={1}
+          slidesPerView={1}
           breakpoints={{
-            640: { slidesPerview: 2 },
-            1400: { slidesPerview: 3 },
+            640: { slidesPerView: 2 },
+            1400: { slidesPerView: 3 },
           }}
           spaceBetween={30}
-          modules={[pagination]}
+          modules={[Pagination]}
           pagination={{
             clickable: true,
           }}
           className="h-[350px]"
         >
           {reviewsData.map((person, index) => {
-            return <SwiperSlide key={index}>
-                <Card>
-
+            return (
+              <SwiperSlide key={index}>
+                <Card className="bg-tertiary dark:bg-secondary/40 p-8 min-h-[300px]">
+                  <CardHeader className="p-0 mb-10">
+                    <div className="flex gap-x-4 items-center">
+                      <Image
+                        src={person.avatar}
+                        width={70}
+                        height={70}
+                        alt="."
+                        priority
+                      />
+                      <div className="flex flex-col">
+                        <CardTitle>{person.name}</CardTitle>
+                        <p>{person.job}</p>
+                      </div>
+                    </div>
+                  </CardHeader>
+                  <CardDescription className="text-lg text-muted-foreground">
+                    {person.review}
+                  </CardDescription>
                 </Card>
-            </SwiperSlide>;
+              </SwiperSlide>
+            );
           })}
         </Swiper>
       </div>

ebbc074d8b94dbf0f8d886800a909263db08643b
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-03-03 22:37:47 +0330

    footer section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,15 @@
+import Socials from "./Socials"
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="bg-secondary py-12">
+      <div className="container mx-auto">
+        <div className="flex items-center justify-center">
+        <Socials  containerStyle="flex gap-x-6 mx-auto xl:mx-0" iconsStyle="text-primary dark:text-white/70 text-[20px] hover:text-white dark:hover:text-primary transition-all"/>
+        </div>
+      </div>
+    </footer>
   )
 }
 
